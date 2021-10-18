812de9d3c43d2d3764aa02e32eba259280027794
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2021-10-18 09:05:35 +0200

    [Lab01] Zad 3 pętlą dla ujemnych potęg

--- Lab01/skrypt03b.sh
+++ Lab01/skrypt03b.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 res=1
 
-for i in $(seq $2); do
-	res=$((res*$1))
-done
+if [ $2 -gt 0 ]; then
+	for i in $(seq $2); do
+		res=$((res*$1))
+	done
+else
+	for i in $(seq $(echo ${2#-}) ); do
+		res=$(echo "$res/$1" | bc -l)
+	done
+fi
 
-echo $res
+echo $res | bc -l
