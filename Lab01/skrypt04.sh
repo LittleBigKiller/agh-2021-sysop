f89fd0ee38e498075377429a37650bd0bdd5554d
LittleBigKiller <35974496+LittleBigKiller@users.noreply.github.com>
2021-10-19 22:58:33 +0200

    [Lab01] Poprawiony skrypt4

--- Lab01/skrypt04.sh
+++ Lab01/skrypt04.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-convert -density 600 $1 temp.png
+convert -density 600 $1 -append temp.png
 composite -gravity northwest -geometry +$3-$4 $2 temp.png final.png
 convert final.png result.pdf
 
